97ad2999d67e4540bb8044ad934b51c6cec14603
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-05 14:59:18 -0500

    added callTimes

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ function isPositive(nums){
 }
 
 myNums = [1,2,3,4,5,6,7,-3,-4,-5];
-console.log(isPositive(myNums));
+// console.log(isPositive(myNums));
 
 
 
@@ -12,16 +12,16 @@ console.log(isPositive(myNums));
 function isEven(nums){
     return nums.filter(num => num % 2 === 0);
 }
-console.log(isEven(myNums));
+// console.log(isEven(myNums));
 
 function squareNumbers(nums){
     let results = [];
     nums.forEach(function(num){
-        results.push(num * num)
+        results.push(num * num);
     });
-    return results
+    return results;
 }
-console.log(squareNumbers(myNums));
+// console.log(squareNumbers(myNums));
 
 const cities = [
     { name: 'Los Angeles', temperature: 60.0},
@@ -30,12 +30,56 @@ const cities = [
     { name: 'New York', temperature: 80.0 }
 ];
 
-function CitiesTemp(info,temp){
-    const tempRequested = num => num < temp;
-    info.forEach(function (myObject){
-        if (tempRequested(myObject.temperature)){
-            console.log(myObject.name)
-        }
+function citiesTemp(info,temp){
+    return info.filter(eaObject => eaObject.temperature < temp);
+}
+// console.log(citiesTemp(cities,70))
+
+function cityNames(info){
+    return info.map(eaObject => eaObject.name);
+}
+// console.log(cityNames(cities))
+
+const people = [
+    'Dom',
+    'Lyn',
+    'Kirk',
+    'Autumn',
+    'Trista',
+    'Jesslyn',
+    'Kevin',
+    'John',
+    'Eli',
+    'Juan',
+    'Robert',
+    'Keyur',
+    'Jason',
+    'Che',
+    'Ben'
+];
+
+function printEveryone(myList){
+    myList.forEach(function (name){
+        console.log(`Good Job, ${name}`);
     })
 }
-CitiesTemp(cities,70)
+// printEveryone(people)
+
+function call3Times(fun) {
+    fun();
+    fun();
+    fun();
+
+}
+function helloWorld(){
+    console.log("hello world");
+}
+// call3Times(helloWorld)
+
+function callNTimes(times, fun){
+    for (let i = 0; i < times; i++){
+        fun()
+    }
+}
+// callNTimes(5, helloWorld)
+
