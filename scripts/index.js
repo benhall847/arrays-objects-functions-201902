e8c3e0a24194026431397cbc6c4367a5df1ab6a4
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-05 13:58:01 -0500

    added isEven challenge, and fixed syntax

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
 // add your solutions here
 function isPositive(nums){
-    results = []
-    const isPositive = (num) => num > 0;
-    results.push(nums.filter(isPositive))
-    return results
+    let results = [];
+    const isPos = (num) => num > 0;
+    results.push(nums.filter(isPos))
+    return results;
 }
-myNums = [1,2,3,4,5,6,7,-3,-4,-5]
-console.log(isPositive(myNums))
+myNums = [1,2,3,4,5,6,7,-3,-4,-5];
+console.log(isPositive(myNums));
+
+function isEven(nums){
+    let results = [];
+    const isEven = (num) => num % 2 === 0;
+    results.push(nums.filter(isEven));
+    return results;
+}
+console.log(isEven(myNums));
