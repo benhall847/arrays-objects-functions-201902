039cc5e9c8b7b04ed2132690b57b13ecc55ecc07
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-05 13:47:55 -0500

    added is positive challenge

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
 // add your solutions here
-console.log("hello")
+function isPositive(nums){
+    result = []
+    nums.forEach(function (num){
+        if (num > 0){
+            result.push(num)
+        }
+    })
+    return result
+}
+myNums = [1,2,3,4,5,-6,-7,-8,3,5,6,-9]
+
+console.log(isPositive(myNums))
