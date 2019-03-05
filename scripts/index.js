f5d9218ac7a3b1569ae0449cefd815d51b74e6ec
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-05 14:12:59 -0500

    modified solutions

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 // add your solutions here
 function isPositive(nums){
-    let results = [];
-    const isPos = (num) => num > 0;
-    results.push(nums.filter(isPos))
-    return results;
+    return nums.filter(num => num > 0);
 }
+
 myNums = [1,2,3,4,5,6,7,-3,-4,-5];
 console.log(isPositive(myNums));
 
+
+
+
 function isEven(nums){
-    let results = [];
-    const isEven = (num) => num % 2 === 0;
-    results.push(nums.filter(isEven));
-    return results;
+    return nums.filter(num => num % 2 === 0);
 }
 console.log(isEven(myNums));
+
+function squareNumbers(nums){
+    let results = [];
+    nums.forEach();
+}
+console.log(squareNumbers(myNums));
